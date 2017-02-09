762aab5cef370191a150c4490957ac057b99cc7b
Zachary Murphy <ztmurphy21@gmail.com>
2017-02-08 19:46:59 -0500

    first script

---
+++ Get-LastBootTime.ps1
@@ -0,0 +1,4 @@
+﻿Get-WmiObject -Class Win32_OperatingSystem –ComputerName localhost |
+
+Select-Object -Property CSName, @{n="Last Booted";
+e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
